8f68082e1ac827d1958e5aa26605bbc0033104dd
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-06-18 23:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object with a property called name
+var student = {
+    name: 'John Doe'
+};
+
+// Add a method to the Object prototype to return an array of all the keys in the object
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Testing the getKeys method
+console.log(student.getKeys()); 
